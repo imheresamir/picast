7038754100743f3d5bc834f544bdd102129fa4fe
Samir Bhatt <sbbhatt8@gmail.com>
2014-11-30 01:48:05 -0800

    Added event source listener to controller

--- static/src/controller.js
+++ static/src/controller.js
@@ -2,86 +2,99 @@
 define(function(require, exports, module) {
     var serverConfig = require('serverConfig');
     var serverIp = 'http://' + serverConfig;
+    var evtSource = new EventSource(serverIp + ':8081/events');
 
     var view = require('main');
     var playlist = view[0];
     var playercontrols = view[1];
-    var uiLock = false;
+    var playLock = true;
 
-    playercontrols._eventInput.on("prev", function() {
-        if(!uiLock) {
-            playlist._eventOutput.emit("prevSelect");
-
-            console.log(playlist.currentlySelectedIndex);
-
-            // Talk to server
-
-            playlist._eventOutput.emit("prevPlay");
+    evtSource.addEventListener("playerStateChanged", function(e) {
+        console.log("Server event: playerState - " + e.data);
+        if(e.data == "stopped") {
+            playLock = true;
+            playercontrols._eventOutput.emit("stopped");
+        } else if(e.data == "paused") {
+            playLock = false;
+            playercontrols._eventOutput.emit("paused");
+        } else if(e.data == "playing") {
+            playLock = false;
+            playercontrols._eventOutput.emit("playing");
         }
     });
 
     playercontrols._eventInput.on("pause", function() {
-        console.log("pause");
-        //uiLock = true;
+        if(!playLock) {
+            console.log("pause");
+            //uiLock = true;
 
-        // Talk to server
-        var ajax = new XMLHttpRequest();
+            // Talk to server
+            var ajax = new XMLHttpRequest();
 
-        ajax.open('POST', serverIp + ':8082/media/pause', true);
-        ajax.setRequestHeader('Content-Type', 'application/json');
-        ajax.send('{"Pause":"pause"}');
-        
-        ajax.onreadystatechange = function() {
-            if(ajax.readyState == 4 && ajax.status == 200) {
-                uiLock = false;
+            ajax.open('GET', serverIp + ':8082/media/pause', true);
+            ajax.setRequestHeader('Content-Type', 'application/json');
+            ajax.send('{"Pause":"pause"}');
+            
+            ajax.onreadystatechange = function() {
+                if(ajax.readyState == 4 && ajax.status == 200) {
+                    uiLock = false;
+                }
             }
-        }
 
-        playercontrols._eventOutput.emit("paused");
+            //playercontrols._eventOutput.emit("paused");
+        }
 
     });
 
     playercontrols._eventInput.on("play", function() {
-        console.log("play");
-        //uiLock = true;
+        if(!playLock) {
+            console.log("play");
+            //uiLock = true;
+
+            // Talk to server
+            var ajax = new XMLHttpRequest();
+
+            ajax.open('GET', serverIp + ':8082/media/pause', true);
+            ajax.setRequestHeader('Content-Type', 'application/json');
+            ajax.send('{"Pause":"play"}');
+            
+            ajax.onreadystatechange = function() {
+                if(ajax.readyState == 4 && ajax.status == 200) {
+                    uiLock = false;
+                }
+            }
+
+            //playercontrols._eventOutput.emit("playing");
+        }
+        
+    });
+
+    playercontrols._eventInput.on("prev", function() {
+        playlist._eventOutput.emit("prevSelect");
+
+        console.log(playlist.currentlySelectedIndex);
 
         // Talk to server
-        var ajax = new XMLHttpRequest();
 
-        ajax.open('POST', serverIp + ':8082/media/pause', true);
-        ajax.setRequestHeader('Content-Type', 'application/json');
-        ajax.send('{"Pause":"play"}');
-        
-        ajax.onreadystatechange = function() {
-            if(ajax.readyState == 4 && ajax.status == 200) {
-                uiLock = false;
-            }
-        }
-
-        playercontrols._eventOutput.emit("playing");
-        
+        playlist._eventOutput.emit("prevPlay");
     });
 
     playercontrols._eventInput.on("next", function() {
-        if(!uiLock) {
-            playlist._eventOutput.emit("nextSelect");
+        playlist._eventOutput.emit("nextSelect");
 
-            console.log(playlist.currentlySelectedIndex);
+        console.log(playlist.currentlySelectedIndex);
 
-            // Talk to server
+        // Talk to server
 
-            playlist._eventOutput.emit("nextPlay");
-        }
+        playlist._eventOutput.emit("nextPlay");
     });
 
     playlist._eventInput.on("itemClicked", function(index) {
-        if(!uiLock) {
-            playlist._eventOutput.emit("select", index);
+        playlist._eventOutput.emit("select", index);
 
-            // Talk to server
+        // Talk to server
 
-            playlist._eventOutput.emit("play", index);
-        }
+        playlist._eventOutput.emit("play", index);
     });
 
 });
