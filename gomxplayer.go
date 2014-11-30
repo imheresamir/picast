f66f3691ab0866a10529dd61736714440fdb540b
Samir Bhatt <sbbhatt8@gmail.com>
2014-11-30 01:43:13 -0800

    Consolidated MediaPlayer interface status codes

--- gomxplayer.go
+++ gomxplayer.go
@@ -24,26 +24,17 @@ import (
 	}
 }*/
 
-func (video *OmxPlayer) CacheFile() string {
-	return video.Outfile
-}
-
-func (video *OmxPlayer) Status() int {
-	return video.Playing
+func (video *OmxPlayer) StatusCode() int {
+	return video.Status
 }
 
 func (video *OmxPlayer) ReturnCode() int {
 	return <-video.KillSwitch
 }
 
-func (video *OmxPlayer) Started() int {
-	return video.ThreadStarted
-}
-
 func (video *OmxPlayer) Play() {
 	if video.Outfile != "" {
-		//video.Started = 0
-		video.ThreadStarted = 0
+		video.Status = 1
 
 		// Start video
 		cmd := exec.Command("omxplayer", "-I", video.Outfile)
@@ -66,7 +57,7 @@ func (video *OmxPlayer) Play() {
 			outBytes, err = exec.Command("util/dbuscontrol.sh", "status").Output()
 
 			if err == nil {
-				//video.Started = 1
+				video.Status = 3
 
 				break
 			} else {
@@ -82,35 +73,13 @@ func (video *OmxPlayer) Play() {
 			log.Fatal(err)
 		}
 
-		// Get Status
-		reg = regexp.MustCompile("Paused: (.+)")
-
-		outBytes, err = exec.Command("util/dbuscontrol.sh", "status").Output()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		outString = string(outBytes[:])
-
-		paused := reg.FindStringSubmatch(outString)[1]
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if paused == "false" {
-			video.Playing = 1
-		} else if paused == "true" {
-			video.Playing = 0
-		}
-
 		// Launch goroutine
-		video.ThreadStarted = 1
 		go video.WatchPosition()
 	}
 }
 
 func (video *OmxPlayer) TogglePause() {
-	if video.ThreadStarted == 1 {
+	if video.Status > 1 {
 
 		for {
 			cmd := exec.Command("util/dbuscontrol.sh", "pause")
@@ -124,10 +93,10 @@ func (video *OmxPlayer) TogglePause() {
 
 		}
 
-		if video.Playing == 1 {
-			video.Playing = 0
-		} else if video.Playing == 0 {
-			video.Playing = 1
+		if video.Status == 3 {
+			video.Status = 2
+		} else if video.Status == 2 {
+			video.Status = 3
 			go video.WatchPosition()
 		}
 	}
@@ -136,29 +105,17 @@ func (video *OmxPlayer) TogglePause() {
 // Stop method can be called internally from WatchPosition with kill signal 1 on normal video end
 // OR externally from Api or Media methods with kill signal -1
 func (video *OmxPlayer) Stop(signal int) {
-	if video.ThreadStarted == 1 {
-		/*for ; video.ThreadStarted != 1; {
-			log.Println("Sleeping in Kill")
-			time.Sleep(500 * time.Millisecond)
-		}*/
-		video.ThreadStarted = 0
+	if video.Status > 0 {
 		time.Sleep(500 * time.Millisecond)
 
 		cmd := exec.Command("killall", "omxplayer", "omxplayer.bin")
 		cmd.Run()
 
-		//video.Outfile = ""
-		//video.Started = 0
-		//video.ThreadStarted = 0
-		//video.Playing = 0
-		//video.Duration = 0
-		//video.Position = 0
-		//video = &OmxPlayer{}
-
 		log.Println("Video stopped.")
 
 		os.Remove(video.Outfile)
 
+		video.Status = 0
 		video.KillSwitch <- signal
 
 	}
@@ -166,11 +123,7 @@ func (video *OmxPlayer) Stop(signal int) {
 
 func (video *OmxPlayer) WatchPosition() {
 	for {
-		if video.ThreadStarted != 1 {
-			break
-		}
-
-		if video.Playing == 0 {
+		if video.Status < 3 {
 			break
 		}
 
