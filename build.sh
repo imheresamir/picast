03e7564b643a9986c106eefb594a01a7fc0ec0a0
Samir Bhatt <sbbhatt8@gmail.com>
2014-12-01 18:02:32 -0800

    Added spotify library files, created build and run scripts

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SPOTIFYLIBS=`pwd`/lib/spotify
+SPOTIFYPCDIR=$SPOTIFYLIBS/lib/pkgconfig
+
+export PKG_CONFIG_PATH=$SPOTIFYPCDIR
+go build -o server.bin -x main/server.go main/ipaddr.go
+
